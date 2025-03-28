97abb0d8b2bcf5be0e2ce2e037504e12105ab4b4
samir <samirgandhi777@gmail.com>
2025-03-28 07:47:30 -0500

    starting the front end

--- T2RFrame.java
+++ T2RFrame.java
@@ -1,3 +1,13 @@
-public class T2RFrame {
-
+import java.awt.*;
+import javax.swing.*;
+public class T2RFrame extends JFrame{
+    private int WIDTH = 1200;
+    private int HEIGHT = 480;
+    public T2RFrame(String s)
+    {
+        super(s);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(WIDTH,HEIGHT);
+    setVisible(true);
+    }
 }
