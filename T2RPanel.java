a7af47b42876ba19aecd25f87fd5f63fc8511700
samir <samirgandhi777@gmail.com>
2025-03-28 08:03:49 -0500

    more front end

--- T2RPanel.java
+++ T2RPanel.java
@@ -1,3 +1,47 @@
-public class T2RPanel {
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.*;
+public class T2RPanel extends JPanel implements MouseListener{
+    public T2RPanel()
+    {
+
+    }
+
+    public void paint(Graphics g)
+    {
+
+        
+
+
+
+
+        
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+       
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+       
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+       
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+       
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
