b278fec0f1874028bbe20d8159b3bedd261daaea
Day (7L) <K1909873@students.katyisd.org>
2025-04-01 07:49:12 -0500

    getting coordinate grid set up, sorry for the inconvinience :D

--- T2RFrame.java
+++ T2RFrame.java
@@ -1,8 +1,8 @@
 import java.awt.*;
 import javax.swing.*;
 public class T2RFrame extends JFrame{
-    private int WIDTH = 1200;
-    private int HEIGHT = 480;
+    private int WIDTH = 1625;
+    private int HEIGHT = 880;
     public T2RFrame(String s)
     {
         super(s);
--- T2RPanel.java
+++ T2RPanel.java
@@ -7,21 +7,39 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 public class T2RPanel extends JPanel implements MouseListener{
     BufferedImage trainBG;
+    Image im;
 
     public T2RPanel()
     {
         System.out.println("testing");
         try {
-            trainBG = ImageIO.read(T2RPanel.class.getResource("backgroundImages\\trainBG.png"));
+            trainBG = ImageIO.read(T2RPanel.class.getResource("Images\\trainBG.png"));
         } catch (Exception e) {
             System.out.println("Exception  ");
         }
+
+        im = ImageLoader.get("/Images/t2r map.png");
+
     }
 
     public void paint(Graphics g)
     {
-            g.drawImage(trainBG, 0, 0, getWidth(), getHeight(), null);
-    }
+            //g.drawImage(trainBG, 0, 0, getWidth(), getHeight(), null);
+
+            g.drawImage(im, 0, 0, getWidth() - 405, getHeight() - 190 ,null);
+
+            g.setColor(Color.cyan);
+            for(int x = 0; x < getWidth(); x += 25){
+                    g.drawLine(x, 0, x, getHeight());
+            }
+
+            g.setColor(Color.magenta);
+            for(int y = 0; y < getHeight(); y += 25){
+                g.drawLine(0, y, getWidth(), y);
+        }
+
+
+    }//end of paint
 
     @Override
     public void mouseClicked(MouseEvent e) {
