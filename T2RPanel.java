774734fedba46603a40fbd6e5a7e07895e0ef561
samir <samirgandhi777@gmail.com>
2025-04-01 07:34:21 -0500

    mouse progress

--- T2RPanel.java
+++ T2RPanel.java
@@ -5,13 +5,18 @@ import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
 public class T2RPanel extends JPanel implements MouseListener{
     BufferedImage trainBG;
+    int gameState;
 
     public T2RPanel()
     {
+        gameState = 0;
         System.out.println();
         System.out.println("testing");
+        
         try {
             trainBG = ImageIO.read(T2RPanel.class.getResource("backgroundImages\\trainBG.png"));
             System.out.println("train");
@@ -29,10 +34,23 @@ public class T2RPanel extends JPanel implements MouseListener{
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        System.out.println("mouse clicked");
-        int x = e.getX();
-        int y= e.getY();
-        System.out.println("x: " + x + ". y: " + y);
+       
+        double x = e.getX();
+        double y= e.getY();
+        System.out.println("x: " + x/getWidth() + ". y: " + y/getHeight());
+
+        if(gameState == 0)
+        {
+            if (rectangularInBounds(x, y, 0.07686*getWidth(), 0.23818*getWidth(), 0.11337*getHeight(), 0.26077*getHeight()))
+            {
+                System.out.println("start game");
+            }
+
+
+
+
+
+        }
     }
 
     @Override
@@ -52,6 +70,20 @@ public class T2RPanel extends JPanel implements MouseListener{
 
     @Override
     public void mouseExited(MouseEvent e) {
+
+    }
+
+    public boolean rectangularInBounds(double x, double y, double minX, double maxX, double minY, double maxY)
+    {
+        if ( minX < x && x < maxX)
+        {
+            if (minY < y && y < maxY)
+            {
+                return true;
+            }
+        }
+        return false;
+
 
     }
 }
