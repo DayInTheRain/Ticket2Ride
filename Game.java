5a2a36d9b771fbab38abcb46aae1805c059023a1
Day (7L) <K1909873@students.katyisd.org>
2025-04-15 08:13:10 -0500

    hi

--- Game.java
+++ Game.java
@@ -5,6 +5,8 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.util.*;
 
+//syncing issues, lovely.
+
 public class Game {
     private boolean lastRound;
     private ArrayList<Player> players;
